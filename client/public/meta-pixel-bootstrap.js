360ed97238e60adb03c347ef77515c602d2e6c5a
Gautam Sharma <GAUTAM.SHRM7004@GMAIL.COM>
2026-08-05 21:52:31 +0530

    Recover Meta library from existing queue

--- client/public/meta-pixel-bootstrap.js
+++ client/public/meta-pixel-bootstrap.js
@@ -1,23 +1,22 @@
 (function (w, d) {
-  if (w.fbq) {
-    d.documentElement.setAttribute('data-cruisin-meta-bootstrap', 'ready');
-    w.dispatchEvent(new Event('cruisin-meta-bootstrap-ready'));
-    return;
-  }
-  var n = w.fbq = function () {
+  var n = w.fbq;
+  if (!n) n = w.fbq = function () {
     n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
   };
   if (!w._fbq) w._fbq = n;
-  n.push = n;
+  if (!n.push) n.push = n;
   n.loaded = true;
-  n.version = '2.0';
-  n.queue = [];
-  var script = d.createElement('script');
-  script.id = 'cruisin-meta-pixel-library';
-  script.async = true;
-  script.src = 'https://connect.facebook.net/en_US/fbevents.js';
-  var firstScript = d.getElementsByTagName('script')[0];
-  firstScript.parentNode.insertBefore(script, firstScript);
+  if (!n.version) n.version = '2.0';
+  if (!n.queue) n.queue = [];
+  var libraryUrl = 'https://connect.facebook.net/en_US/fbevents.js';
+  if (!n.callMethod && !d.querySelector('script[src="' + libraryUrl + '"]')) {
+    var script = d.createElement('script');
+    script.id = 'cruisin-meta-pixel-library';
+    script.async = true;
+    script.src = libraryUrl;
+    var firstScript = d.getElementsByTagName('script')[0];
+    firstScript.parentNode.insertBefore(script, firstScript);
+  }
   d.documentElement.setAttribute('data-cruisin-meta-bootstrap', 'ready');
   w.dispatchEvent(new Event('cruisin-meta-bootstrap-ready'));
 })(window, document);
